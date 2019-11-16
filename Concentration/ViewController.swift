745d5532f5ff22c017634fc8ba9b01496d48fe38
To Hoang Linh <tohoanglinh@gmail.com>
2019-11-16 21:25:53 +0700

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by ToHoangLinh on 11/16/19.
+//  Copyright © 2019 Freelancer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
